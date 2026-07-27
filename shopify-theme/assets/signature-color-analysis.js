308e3421fc13022870a813fb717cd15a2d2f014c
Jen Vax <jenvax@mac.myfiosgateway.com>
2026-07-27 13:33:23 -0400

    Match custom palette customer context in studio

--- shopify-theme/assets/signature-color-analysis.js
+++ shopify-theme/assets/signature-color-analysis.js
@@ -765,7 +765,7 @@ async function drawRealisticDrapeTexture(ctx, options) {
     const query = new URLSearchParams();
     if (action) query.set('action', action);
     query.set('customerId', VIEWER_CUSTOMER_ID);
-    if (ADMIN_CUSTOMER_ID) query.set('previewCustomerId', ADMIN_CUSTOMER_ID);
+    query.set('previewCustomerId', ADMIN_CUSTOMER_ID || VIEWER_CUSTOMER_ID);
     if (ADMIN_VIEW_AS) query.set('viewAs', ADMIN_VIEW_AS);
     return APP_BASE_URL + '/api/custom-palettes?' + query.toString();
   }
